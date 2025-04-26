ae6154e0111814b71ae02982fefef02b688c4a56
kostiantynkovalchuk <kostiantynkovalchuk32@gmail.com>
2025-04-26 11:37:15 +0200

    decrement button added

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -1,7 +1,13 @@
-//increments the number in a node's text
+// increments the number in a node's text
 function increment(node) {
   let current = node.textContent;
   node.textContent = Number(current) + 1;
+}
+
+// decrements the number in a node's text
+function decrement(node) {
+  let current = node.textContent;
+  node.textContent = Number(current) - 1;
 }
 
 export function App() {
@@ -9,22 +15,35 @@ export function App() {
 
   const header = document.createElement("header");
   header.innerHTML = `
-        <h1>Number Counter</h1>
-        <p>A simple counter. Press increment to increase the count by one.</p>
-    `;
+    <h1>Number Counter</h1>
+    <p>A simple counter. Press increment to increase or decrement to decrease the count by one.</p>
+  `;
   body.appendChild(header);
 
   const main = document.createElement("main");
   main.innerHTML = `
-        <p id="counter" data-testid="counter">0</p>
-        <button id="increment">Increment</button>
-    `;
+    <p id="counter" data-testid="counter">0</p>
+    <button id="increment">Increment</button>
+    <button id="increment2">Increment again</button>
+    <button id="decrement">Decrement</button>
+  `;
   body.appendChild(main);
 
-  const button = body.querySelector("#increment");
   const counter = body.querySelector("#counter");
-  button.addEventListener("click", () => {
+  const incrementButton = body.querySelector("#increment");
+  const incrementButton2 = body.querySelector("#increment2");
+  const decrementButton = body.querySelector("#decrement");
+
+  incrementButton.addEventListener("click", () => {
     increment(counter);
+  });
+
+  incrementButton2.addEventListener("click", () => {
+    increment(counter);
+  });
+
+  decrementButton.addEventListener("click", () => {
+    decrement(counter);
   });
 
   return body;
